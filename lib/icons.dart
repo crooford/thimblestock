dd5573448dd1904f3787ed4106eb191bc98d19af
Leonnarddo <leonnarddo@gmail.com>
2022-10-26 08:37:59 -0500

    Agregué archivo de Iconos de la App

---
+++ lib/icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class ThimbleStockIcons {
+  ThimbleStockIcons._();
+
+  static const _kFontFam = 'ThimbleStockIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData home = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData homeactivo = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData proyectos = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData proyectosactivo = IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData catalogo = IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData catalogoactivo = IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData clientes = IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData clientesactivo = IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
