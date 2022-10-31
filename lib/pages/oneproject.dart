886fd048b1a964ed23ccd7749cf4eef7fbfc7f11
Carlos Escudero <escuderocarlos23@gmail.com>
2022-10-31 10:46:01 -0500

    projectpage calendar

---
+++ lib/pages/oneproject.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class OneProjectPage extends StatefulWidget {
+  const OneProjectPage
+({super.key});
+
+  @override
+  State<OneProjectPage> createState() => _OneProjectPageState();
+}
+
+class _OneProjectPageState extends State<OneProjectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:
+          AppBar(
+          
+        automaticallyImplyLeading: false,
+        title: const Text(
+          'Proyectos',
+          style: TextStyle(
+                fontFamily: 'Poppins',
+                color: Colors.white,
+                fontSize: 22,
+              ),
+        ),
+        
+        flexibleSpace: FlexibleSpaceBar(
+          background: Image.asset(
+            'assets/appbarlogo.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            TableCalendar(
+              firstDay: DateTime.utc(2010,01,01),
+              lastDay: DateTime.utc(2040,12,31),
+              focusedDay: DateTime.now(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/projects.dart
+++ lib/pages/projects.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:thimblestock/pages/oneproject.dart';
 
 
 
 
 class ProjectsPage extends StatelessWidget {
   const ProjectsPage ({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+    final lista = _listProjects();
     return Scaffold(
       appBar:
           AppBar(
@@ -32,13 +34,70 @@ class ProjectsPage extends StatelessWidget {
         elevation: 0,
       ), // Reemplazar por appBar widget
       // Aqui va el cuerpo de la app
-      body: const Center(
-        child: Text(
-          "Proyectos",
-          style: TextStyle(fontSize: 30),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Listado de Proyectos",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),Row(
+              children: const[Text("Barra de Busqueda"),],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: lista.length,
+                itemBuilder: (context, index) => ListTile(
+                  leading: const CircleAvatar(),
+                  title: Text(lista[index]),
+                  subtitle: const Text("318 000 000"),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.phone),
+                    onPressed: () {
+                      // TODO Realizar la llamada Telefonica
+                    },
+                  ),
+                  onTap: () {
+                    // TODO: Debe redirigir a los datos del cliente especifico
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const OneProjectPage(),
+                      ),
+                    );
+                  }, 
+                ),
+              ),
+            )
+          ],
         ),
       ),
-       
+
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add_card),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const OneProjectPage(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  List<String> _listProjects() {
+    //TODO:  Base de Datos
+
+    return List<String>.generate(
+      5,
+      (index) => "Cliente ${index + 1}",
     );
   }
 }
