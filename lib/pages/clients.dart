0668a7fe8f5e2b56d1e51255e364108ddb262d30
Leonnarddo <leonnarddo@gmail.com>
2022-10-31 19:13:50 -0500

    clientes-y-dashboard

--- lib/pages/clients.dart
+++ lib/pages/clients.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:thimblestock/icons.dart';
 
 import 'oneclient.dart';
 
@@ -8,82 +7,117 @@ import 'oneclient.dart';
 class ClientsPage extends StatelessWidget {
   const ClientsPage({super.key});
 
+  Future<bool> _onWillPop(BuildContext context) async {
+    bool? exitResult = await showDialog(
+      context: context,
+      builder: (context) => _buildExitDialog(context),
+    );
+    return exitResult ?? false;
+  }
+
+  Future<bool?> _showExitDialog(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      builder: (context) => _buildExitDialog(context),
+    );
+  }
+
+  AlertDialog _buildExitDialog(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Está seguro?'),
+      content: const Text('Desea cerrar Thimblestock?'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: const Text('No'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(true),
+          child: const Text('Si'),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final lista = _listClients();
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text(
-          'Clientes',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            color: Colors.white,
-            fontSize: 22,
-          ),
-        ),
-        flexibleSpace: FlexibleSpaceBar(
-          background: Image.asset(
-            'assets/appbarlogo.png',
-            fit: BoxFit.cover,
-          ),
-        ),
-        centerTitle: true,
-        elevation: 0,
-      ), // Reemplazar por appBar widget
-      // Aqui va el cuerpo de la app
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "Listado de Clientes",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text(
+            'Clientes',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              color: Colors.white,
+              fontSize: 22,
             ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: lista.length,
-                itemBuilder: (context, index) => ListTile(
-                  leading: const CircleAvatar(),
-                  title: Text(lista[index]),
-                  subtitle: const Text("318 000 000"),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.phone),
-                    onPressed: () {
-                      // TODO Realizar la llamada Telefonica
-                    },
-                  ),
-                  onTap: () {
-                    // TODO: Debe redirigir a los datos del cliente especifico
-
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const OneClientPage(),
-                      ),
-                    );
-                  },
+          ),
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.asset(
+              'assets/appbarlogo.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          centerTitle: true,
+          elevation: 0,
+        ), // Reemplazar por appBar widget
+        // Aqui va el cuerpo de la app
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Listado de Clientes",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            )
-          ],
-        ),
-      ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: lista.length,
+                  itemBuilder: (context, index) => ListTile(
+                    leading: const CircleAvatar(),
+                    title: Text(lista[index]),
+                    subtitle: const Text("318 000 000"),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.phone),
+                      onPressed: () {
+                        // TODO Realizar la llamada Telefonica
+                      },
+                    ),
+                    onTap: () {
+                      // TODO: Debe redirigir a los datos del cliente especifico
 
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add_card),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const OneClientPage(),
-            ),
-          );
-        },
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const OneClientPage(),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add_card),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const OneClientPage(),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
