c51c4d2325b6209f003da4eb07e2988aacb56ad6
Daniela Gil Varela <34409926+Nali95@users.noreply.github.com>
2022-11-06 11:36:03 -0500

    add change the collage

--- lib/view/pages/catalog.dart
+++ lib/view/pages/catalog.dart
@@ -1,87 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:thimblestock/view/pages/clients.dart';
+import '../widgets/customAppBar.dart';
 import 'onecatalog.dart';
 
-//import '../widgets/barraNavAbajo.dart';
-
+//import '../widgets/barraNavAbajo.dart';\
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
+
+  Future<bool> _onWillPop(BuildContext context) async {
+    bool? exitResult = await showDialog(
+      context: context,
+      builder: (context) => _buildExitDialog(context),
+    );
+    return exitResult ?? false;
+  }
+
+  Future<bool?> _showExitDialog(BuildContext context) async {
+    return await showDialog(
+      context: context,
+      builder: (context) => _buildExitDialog(context),
+    );
+  }
+
+  AlertDialog _buildExitDialog(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Está seguro?'),
+      content: const Text('Desea cerrar Thimblestock?'),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(false),
+          child: const Text('No'),
+        ),
+        TextButton(
+          onPressed: () => Navigator.of(context).pop(true),
+          child: const Text('Si'),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     var lista = _listCatalogo();
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text(
-          'Catalogo',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            color: Colors.white,
-            fontSize: 22,
-          ),
-        ),
-        flexibleSpace: FlexibleSpaceBar(
-          background: Image.asset(
-            'assets/appbarlogo.png',
-            fit: BoxFit.cover,
-          ),
-        ),
-        centerTitle: true,
-        elevation: 0,
-      ), // Reemplazar por appBar widget
-      // Aqui va el cuerpo de la app
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(12, 12, 16, 12),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: lista.length,
-              itemBuilder: (context, index) => ListTile(
-                trailing: IconButton(
-                  icon: Image.asset(
-                    'assets/catalogo/blusa.png',
-                    fit: BoxFit.cover,
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        appBar:
+            CusAppBar(pageTitle: "Catalogo"), // Reemplazar por appBar widget
+        // Aqui va el cuerpo de la app
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(12, 12, 16, 12),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: lista.length,
+                itemBuilder: (context, index) => ListTile(
+                  trailing: iconscatalogo(lista[index]),
+                  //   IconButton(
+                  //   icon: Image.asset(
+                  //     'assets/catalogo/blusa.png',
+                  //     fit: BoxFit.cover,
+                  //   ),
+                  //   onPressed: () {},
+                  // ),
+                  title: Text(
+                    lista[index],
                   ),
-                  onPressed: () {},
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => OneCatalogPage(lista[index]),
+                      ),
+                    );
+                  },
                 ),
-                title: Text(
-                  lista[index],
-                ),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const OneCatalogPage(),
-                    ),
-                  );
-                },
               ),
             ),
-          ),
-        ]),
+          ]),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add_card),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const ClientsPage(),
+              ),
+            );
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add_card),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const OneCatalogPage(),
-            ),
-          );
-        },
-      ),
-      
     );
   }
 
   List _listCatalogo() {
-    List lista = ['Blusas', 'Camisas', 'Jeans', 'Vestidos'];
+    List lista = ['Blusas', 'Camisas', 'Pantalones', 'Vestidos'];
     // return List<String>.generate(
     //   5,
     //   (index) => "Blusas, Polos, Jeans, Pantalones, chaquetas",
     // );
     return lista;
-    
   }
+
+  iconscatalogo(lista) {
+    if('Blusas' == lista) {
+      return IconButton(
+        icon: Image.asset(
+          'assets/catalogo/blusa.png',
+          fit: BoxFit.cover,
+        ),
+        onPressed: () {},
+      );
+    }
+    if('Camisas' == lista) {
+      return IconButton(
+        icon: Image.asset(
+          'assets/catalogo/camisa.png',
+          fit: BoxFit.cover,
+        ),
+        onPressed: () {},
+      );
+    }
+    if('Pantalones' == lista) {
+      return IconButton(
+        icon: Image.asset(
+          'assets/catalogo/pantalones.png',
+          fit: BoxFit.cover,
+        ),
+        onPressed: () {},
+      );
+    }
+    if('Vestidos' == lista) {
+      return IconButton(
+        icon: Image.asset(
+          'assets/catalogo/vestido.png',
+          fit: BoxFit.cover,
+        ),
+        onPressed: () {},
+      );
+    }
+  }
+
 }
--- lib/view/pages/onecatalog.dart
+++ lib/view/pages/onecatalog.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masonry_view/flutter_masonry_view.dart';
+import 'package:thimblestock/view/pages/clients.dart';
+
+import '../widgets/customAppBar.dart';
 
 class OneCatalogPage extends StatelessWidget {
-  const OneCatalogPage({Key? key}) : super(key: key);
-
+  dynamic lista;
+  OneCatalogPage(this.lista, {Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: _OneCatalogPageState(),
+      debugShowCheckedModeBanner: false,
+      home: _OneCatalogPageState(lista),
     );
   }
 }
 
 class _OneCatalogPageState extends StatelessWidget {
-  _OneCatalogPageState({Key? key}) : super(key: key);
-
+  dynamic lista;
+  _OneCatalogPageState(this.lista, {Key? key}) : super(key: key);
   final _blusas = [
     'assets/catalogo/blusas/blusas 1.png',
     'assets/catalogo/blusas/blusas 2.png',
@@ -23,50 +28,94 @@ class _OneCatalogPageState extends StatelessWidget {
     'assets/catalogo/blusas/blusas 5.png',
     'assets/catalogo/blusas/blusas 6.png',
   ];
+  final _camisas = [
+    'assets/catalogo/camisas/camisas 1.png',
+    'assets/catalogo/camisas/camisas 2.png',
+    'assets/catalogo/camisas/camisas 3.png',
+    'assets/catalogo/camisas/camisas 4.png',
+    'assets/catalogo/camisas/camisas 5.png',
+  ];
+  final _pantalones = [
+    'assets/catalogo/pantalones/pantalones 1.png',
+    'assets/catalogo/pantalones/pantalones 2.png',
+    'assets/catalogo/pantalones/pantalones 3.png',
+    'assets/catalogo/pantalones/pantalones 4.png',
+    'assets/catalogo/pantalones/pantalones 5.png',
+  ];
+   final _vestidos = [
+    'assets/catalogo/vestidos/vestidos 1.png',
+    'assets/catalogo/vestidos/vestidos 2.png',
+    'assets/catalogo/vestidos/vestidos 3.png',
+    'assets/catalogo/vestidos/vestidos 4.png',
+    'assets/catalogo/vestidos/vestidos 5.png',
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text(
-          'PRENDAS',
-          style: TextStyle(
-            fontFamily: 'Poppins',
-            color: Colors.white,
-            fontSize: 22,
-          ),
-        ),
-        flexibleSpace: FlexibleSpaceBar(
-          background: Image.asset(
-            'assets/appbarlogo.png',
-            fit: BoxFit.cover,
-          ),
-        ),
-        centerTitle: true,
-        elevation: 0,
-      ),
+      appBar: CusAppBar(pageTitle: 'Prendas'),
       body: SingleChildScrollView(
-        child: MasonryView(
-          listOfItem: _blusas,
-          numberOfColumn: 2,
-          itemBuilder: (item) {
-            return Image.asset(item);
-          },
-        ),
+        child: catalogos(lista),
+        // MasonryView(
+
+        //   listOfItem: _blusas,
+        //   numberOfColumn: 2,
+        //   itemBuilder: (item) {
+        //     return Image.asset(item);
+        //   },
+        // ),
       ),
       floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add_card),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const OneCatalogPage(),
-              ),
-            );
-          },
-        ),
+        child: const Icon(Icons.add_card),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const ClientsPage(),
+            ),
+          );
+        },
+      ),
     );
   }
-}
 
+  catalogos(lista) {
+    if ('Blusas' == lista) {
+      return MasonryView(
+        listOfItem: _blusas,
+        numberOfColumn: 2,
+        itemBuilder: (item) {
+          return Image.asset(item);
+        },
+      );
+    }
+    if ('Camisas' == lista) {
+      return MasonryView(
+        listOfItem: _camisas,
+        numberOfColumn: 2,
+        itemBuilder: (item) {
+          return Image.asset(item);
+        },
+      );
+    }
+    if ('Pantalones' == lista) {
+      return MasonryView(
+        listOfItem: _pantalones,
+        numberOfColumn: 2,
+        itemBuilder: (item) {
+          return Image.asset(item);
+        },
+      );
+    }
+    if ('Vestidos' == lista) {
+      return MasonryView(
+        listOfItem: _vestidos,
+        numberOfColumn: 2,
+        itemBuilder: (item) {
+          return Image.asset(item);
+        },
+      );
+    }
+  }
+}
