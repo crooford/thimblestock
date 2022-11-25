867addb9ce67411ce11e21789af8c13cc1c74e10
Leonnarddo <leonnarddo@gmail.com>
2022-11-25 16:01:43 -0500

    Hotfix catalog

--- lib/view/pages/catalog.dart
+++ lib/view/pages/catalog.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thimblestock/model/entity/catalog.dart';
+import '../../controller/activity.dart';
 import '../../controller/catalog.dart';
 import '../../controller/projects.dart';
+import '../../model/entity/activity.dart';
 import '../widgets/customAppBar.dart';
 import 'newcatalog.dart';
 import 'onecatalog.dart';
 
 //import '../widgets/barraNavAbajo.dart';\
 class CatalogPage extends StatefulWidget {
-  const CatalogPage({super.key});
+ final _pref = SharedPreferences.getInstance();
+  late final CatalogEntity _catalogEntity;
+  late final ActivityEntity _activity;
+  late final ActivityController _activitycontroller;
+  final String action = "deleteClient";
+
+
+  CatalogPage({super.key}) {   
+    _catalogEntity = CatalogEntity();
+    _activitycontroller = ActivityController();
+    _activity = ActivityEntity();
+    _pref.then((pref) {
+      _catalogEntity.user = pref.getString("uid");
+      _activity.user = pref.getString("uid");
+    });}
+
+
   @override
   State<CatalogPage> createState() => _CatalogPageState();
 }
-class _CatalogPageState extends State<CatalogPage>{
-  List<CatalogEntity> _listCatalog = [];
+
+class _CatalogPageState extends State<CatalogPage> {
+  List<CatalogEntity> lista = [];
   final _pref = SharedPreferences.getInstance();
-  final _CatalogController = CatalogController();
+  final _catalogController = CatalogController();
+  final _activitycontroller = ActivityController();
+  final _activity = ActivityEntity();
 
   @override
   void initState() {
@@ -27,22 +48,17 @@ class _CatalogPageState extends State<CatalogPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CusAppBar(
-        pageTitle: "Catalogo"
-        ),
+      appBar: CusAppBar(pageTitle: "Catalogo"),
       // Aqui va el cuerpo de la app
       body: Padding(
         padding: const EdgeInsets.fromLTRB(12, 12, 16, 12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start, 
-          children: [
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           Expanded(
             child: ListView.builder(
-              itemCount: _listCatalog.length,
+              itemCount: lista.length,
               itemBuilder: (context, index) => ListTile(
-
-                title: Text(_listCatalog[index].addclass!),
-                trailing: iconscatalogo(_listCatalog[index].addclass!),
+                title: Text(lista[index].addclass!),
+                trailing: iconscatalogo(lista[index].addclass!),
                 //   IconButton(
                 //   icon: Image.asset(
                 //     'assets/catalogo/blusa.png',
@@ -54,7 +70,7 @@ class _CatalogPageState extends State<CatalogPage>{
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => OneCatalogPage(_listCatalog[index]),
+                      builder: (context) => OneCatalogPage(lista, catalogEntity: lista[index] ),
                     ),
                   );
                 },
@@ -65,71 +81,73 @@ class _CatalogPageState extends State<CatalogPage>{
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.shopping_basket_rounded),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => NewCatalogPage(),
-            )
-          );
+        onPressed: () async {
+          await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NewCatalogPage(),
+              ));
+          setState(() {
+            _listCatalogo();
+          });
         },
       ),
     );
   }
 
   void _listCatalogo() {
-  _pref.then((pref) {
+    _pref.then((pref) {
       var id = pref.getString("uid") ?? "";
-      _CatalogController.listAll(id).then((value) {
+      _catalogController.listAll(id).then((value) {
         setState(() {
-          _listCatalog = value;
+          lista = value;
         });
       });
     });
   }
-    // List lista = ['Blusas', 'Camisas', 'Pantalones', 'Vestidos'];
-    // // return List<String>.generate(
-    // //   5,
-    // //   (index) => "Blusas, Polos, Jeans, Pantalones, chaquetas",
-    // // );
-    // return lista;
-  }
+  // List lista = ['Blusas', 'Camisas', 'Pantalones', 'Vestidos'];
+  // // return List<String>.generate(
+  // //   5,
+  // //   (index) => "Blusas, Polos, Jeans, Pantalones, chaquetas",
+  // // );
+  // return lista;
+}
 
-  iconscatalogo(_listCatalog) {
-    if ('Blusas' == _listCatalog) {
-      return IconButton(
-        icon: Image.asset(
-          'assets/catalogo/blusa.png',
-          fit: BoxFit.cover,
-        ),
-        onPressed: () {},
-      );
-    }
-    if ('Camisas' == _listCatalog) {
-      return IconButton(
-        icon: Image.asset(
-          'assets/catalogo/camisa.png',
-          fit: BoxFit.cover,
-        ),
-        onPressed: () {},
-      );
-    }
-    if ('Pantalones' == _listCatalog) {
-      return IconButton(
-        icon: Image.asset(
-          'assets/catalogo/pantalones.png',
-          fit: BoxFit.cover,
-        ),
-        onPressed: () {},
-      );
-    }
-    if ('Vestidos' == _listCatalog) {
-      return IconButton(
-        icon: Image.asset(
-          'assets/catalogo/vestido.png',
-          fit: BoxFit.cover,
-        ),
-        onPressed: () {},
-      );
-    }
+iconscatalogo(lista) {
+  if ('Blusas' == lista) {
+    return IconButton(
+      icon: Image.asset(
+        'assets/catalogo/blusa.png',
+        fit: BoxFit.cover,
+      ),
+      onPressed: () {},
+    );
+  }
+  if ('Camisas' == lista) {
+    return IconButton(
+      icon: Image.asset(
+        'assets/catalogo/camisa.png',
+        fit: BoxFit.cover,
+      ),
+      onPressed: () {},
+    );
+  }
+  if ('Pantalones' == lista) {
+    return IconButton(
+      icon: Image.asset(
+        'assets/catalogo/pantalones.png',
+        fit: BoxFit.cover,
+      ),
+      onPressed: () {},
+    );
+  }
+  if ('Vestidos' == lista) {
+    return IconButton(
+      icon: Image.asset(
+        'assets/catalogo/vestido.png',
+        fit: BoxFit.cover,
+      ),
+      onPressed: () {},
+    );
+  }
 }
